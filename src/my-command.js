6bc9b6961756a3e44505b3dd3d9e5104fa16c4fd
lizheming <i@imnerd.org>
2020-10-05 23:37:43 +0800

    增加插件回调

--- src/my-command.js
+++ src/my-command.js
@@ -1,6 +1,7 @@
-import sketch from 'sketch'
+import sketch, { Image, Types, Rectangle } from 'sketch/dom'
 // documentation: https://developer.sketchapp.com/reference/api/
 import BrowserWindow from 'sketch-module-web-view';
+
 export default function() {
   const browserWindow = new BrowserWindow({
     width: 320,
@@ -11,5 +12,37 @@ export default function() {
     maximizable: false,
     minimizable: false
   });
-  browserWindow.loadURL(require('../resources/webview.html'))
+  browserWindow.webContents.on('insert', function(width, height, category, filter) {
+    const artboard = getSelectedArtboard();
+    if(!artboard) {
+      return sketch.UI.message('你没有选择画板');
+    }
+    const url = 'https://placeimg.com/' + [width, height, category, filter].join('/');
+    new Image({
+      image:  NSURL.URLWithString(url),
+      parent: artboard,
+      frame: new Rectangle(0, 0, width, height),
+    });
+    return browserWindow.close();
+  });
+  browserWindow.loadURL(require('../resources/webview.html'));
+}
+
+function getFirstArtboard() {
+  const page = sketch.getSelectedDocument()?.pages[0];
+  return page.layers.filter(({type}) => type === Types.Artboard);
+}
+
+function getSelectedArtboard() {
+  const document = sketch.getSelectedDocument();
+  if(!document) {
+    return getFirstArtboard();
+  }
+  const { selectedLayers } = document;
+  const artboards = selectedLayers.layers.filter(({type}) => type === Types.Artboard);
+  if(artboards.length) {
+    return artboards[0];
+  }
+  const parentArtboards = selectedLayers.map(layer => layer.getParentArtboard?.()).filter(Boolean);
+  return [...new Set(parentArtboards)][0] ?? getFirstArtboard();
 }
