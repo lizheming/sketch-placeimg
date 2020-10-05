191b3b618827a8aec3bcb25f3aa958d93f57b1ff
lizheming <i@imnerd.org>
2020-10-05 22:05:06 +0800

    react init

---
+++ resources/webview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (<>
+    <p>Hello World!</p>
+    <hr />
+    via: <em>@lizheming</em>
+  </>)
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
